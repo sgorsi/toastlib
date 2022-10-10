d8674f818c447b41a709519c6a1b8cf5abea7717
shafiq <technodroidzllp@gmail.com>
2022-10-10 19:08:46 +0530

    first commit

--- app/src/main/java/com/techd/mylibrary/MainActivity.java
+++ app/src/main/java/com/techd/mylibrary/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Unknown.justToast(this,"Yup! the toast is grenerated");
     }
 }
